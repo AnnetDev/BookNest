4f29bb5255cf95417a922d4e8b757ef1a5722577
Ebrima-Sarge <ebrimasarge21@gmail.com>
2025-12-03 00:27:05 +0100

    fixed the drop menu z-index

--- src/header-menu.js
+++ src/header-menu.js
@@ -1,9 +1,9 @@
  document.addEventListener("DOMContentLoaded", function() {
-        // 1. Select the elements
+        // Select the elements
         const menuIcon = document.querySelector(".icon-menu");
         const subMenu = document.getElementById("subMenu");
 
-        // 2. Add the click event listener
+        // Add the click event listener
         menuIcon.addEventListener("click", function() {
             // This adds/removes the class that sets max-height to 400px
             subMenu.classList.toggle("open-menu");
