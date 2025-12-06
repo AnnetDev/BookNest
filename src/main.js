9312de0717a6f8221851c25287e93cad02389b3e
Yuliia <yublagov@gmail.com>
2025-12-06 16:37:09 +0100

    add shopping card

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './styles/main.scss';
-import './scripts/all-books.js';
-import './scripts/hero-slider.js';
 import './header-menu';
+import './scripts/hero-slider.js';
 import './scripts/bestsellers.js';
+import './scripts/all-books.js';
 import { clearCart, updateCartCount } from "./scripts/cartService.js";
    window.addEventListener("DOMContentLoaded", () => {
     updateCartCount();
