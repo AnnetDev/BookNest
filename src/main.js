741edad91b2693694ef7e896d543134267ec7a5b
Ebrima-Sarge <ebrimasarge21@gmail.com>
2025-12-09 16:05:48 +0100

    cerated the save button fuctionality.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import './scripts/hero-slider.js';
 import './header-menu';
 import './scripts/bestsellers.js';
 import { clearCart, updateCartCount } from "./scripts/cartService.js";
+import { updateSavedCount } from './scripts/save.js';
    window.addEventListener("DOMContentLoaded", () => {
     updateCartCount();
+    updateSavedCount();
     clearCart();
     
 });
+
--- src/scripts/hero-slider.js
+++ src/scripts/hero-slider.js
@@ -1,105 +1,105 @@
-import Swiper from 'swiper';
-import { Autoplay, EffectCoverflow } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { getBooks } from './bookService.js';
+    import Swiper from 'swiper';
+    import { Autoplay, EffectCoverflow } from 'swiper/modules';
+    import 'swiper/css';
+    import 'swiper/css/navigation';
+    import 'swiper/css/pagination';
+    import { getBooks } from './bookService.js';
 
-const loader = document.querySelector('.lds-roller');
-const swiperWrapper = document.querySelector('.swiper-wrapper');
+    const loader = document.querySelector('.lds-roller');
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
 
 
 
-function createSlides(books) {
-const numSlides = books.length;
+    function createSlides(books) {
+    const numSlides = books.length;
 
-for (let i = 0; i < numSlides; i++) {
-    const slide = document.createElement('div');
-    slide.classList.add('swiper-slide');
-    swiperWrapper.appendChild(slide);
-}
-}
+    for (let i = 0; i < numSlides; i++) {
+        const slide = document.createElement('div');
+        slide.classList.add('swiper-slide');
+        swiperWrapper.appendChild(slide);
+    }
+    }
 
-async function getHeroBooks() {
-    const books = await getBooks();                     
+    async function getHeroBooks() {
+        const books = await getBooks();                     
 
-    const heroBooks = books.sort(() => 0.5 - Math.random()).slice(0, 10);
+        const heroBooks = books.sort(() => 0.5 - Math.random()).slice(0, 10);
 
-    createSlides(heroBooks);                              
+        createSlides(heroBooks);                              
 
-    const slides = document.querySelectorAll('.swiper-slide');  
+        const slides = document.querySelectorAll('.swiper-slide');  
 
-    slides.forEach(slide => {
-        const contentContainer = document.createElement('div');
-        contentContainer.classList.add('slide-content');
-        slide.appendChild(contentContainer);
+        slides.forEach(slide => {
+            const contentContainer = document.createElement('div');
+            contentContainer.classList.add('slide-content');
+            slide.appendChild(contentContainer);
 
-        const imgContainer = document.createElement('div');
-        imgContainer.classList.add('slide-image');
-        contentContainer.appendChild(imgContainer);
+            const imgContainer = document.createElement('div');
+            imgContainer.classList.add('slide-image');
+            contentContainer.appendChild(imgContainer);
 
-        const textContainer = document.createElement('div');
-        textContainer.classList.add('slide-text');
-        contentContainer.appendChild(textContainer);
+            const textContainer = document.createElement('div');
+            textContainer.classList.add('slide-text');
+            contentContainer.appendChild(textContainer);
 
-        const promoText = document.createElement('h2');
-        promoText.textContent = 'Black Week!';
-        textContainer.appendChild(promoText);
+            const promoText = document.createElement('h2');
+            promoText.textContent = 'Black Week!';
+            textContainer.appendChild(promoText);
 
-        const promoTextDescription = document.createElement('h3');
-        promoTextDescription.textContent = 'Up to 50%!';
-        textContainer.appendChild(promoTextDescription);
-    });
+            const promoTextDescription = document.createElement('h3');
+            promoTextDescription.textContent = 'Up to 50%!';
+            textContainer.appendChild(promoTextDescription);
+        });
 
-    slides.forEach((slide, index) => {
-        const imgSrc = heroBooks[index].formats['image/jpeg'] || '';
-        const imgContainer = slide.querySelector('.slide-image');
+        slides.forEach((slide, index) => {
+            const imgSrc = heroBooks[index].formats['image/jpeg'] || '';
+            const imgContainer = slide.querySelector('.slide-image');
 
-        const imgElement = document.createElement('img');
-        imgElement.src = imgSrc;
-        imgElement.alt = heroBooks[index].title;
+            const imgElement = document.createElement('img');
+            imgElement.src = imgSrc;
+            imgElement.alt = heroBooks[index].title;
 
-        imgContainer.appendChild(imgElement);
-    });
+            imgContainer.appendChild(imgElement);
+        });
 
-    loader.remove();
-}
+        loader.remove();
+    }
 
-getHeroBooks();
+    getHeroBooks();
 
-// init Swiper:
-const heroSwiper = new Swiper('.swiper', {
-    modules: [Autoplay, EffectCoverflow],
-    slidesPerView: 1,
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        type: 'bullets',
-    },
-    slidesPerView: 1.3,
-    spaceBetween: 30,
-
-    a11y: {
-        prevSlideMessage: 'Previous slide',
-        nextSlideMessage: 'Next slide',
-    },
-
-    speed: 1000,
-
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-
-    breakpoints: {
-        768: {
-            slidesPerView: 2.3,   
+    // init Swiper:
+    const heroSwiper = new Swiper('.swiper', {
+        modules: [Autoplay, EffectCoverflow],
+        slidesPerView: 1,
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'bullets',
         },
-        1024: {
-            slidesPerView: 3.3, 
-        },
-    },
+        slidesPerView: 1.3,
+        spaceBetween: 30,
 
-});
+        a11y: {
+            prevSlideMessage: 'Previous slide',
+            nextSlideMessage: 'Next slide',
+        },
+
+        speed: 1000,
+
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2.3,   
+            },
+            1024: {
+                slidesPerView: 3.3, 
+            },
+        },
+
+    });
