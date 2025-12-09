ab11a0563d43331eef33763f3e1ec2012dac7fed
Yuliia <yublagov@gmail.com>
2025-12-09 20:46:38 +0100

    add total count

--- src/scripts/shopping-cart.js
+++ src/scripts/shopping-cart.js
@@ -2,10 +2,11 @@ import { getCart } from "./cartService";
 
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("cart-items-container");
+    const totalEl = document.getElementById("cart-total-amount");
     if (!container) return;
 
     const books = getCart();
-    console.log("Books from cart:", books);
+    let total = 0;
 
     books.forEach(book => {
         const item = document.createElement("div");
@@ -14,24 +15,34 @@ document.addEventListener("DOMContentLoaded", () => {
         item.innerHTML = `<img src="${book.img}" alt="${book.title}" style="width:90px;">
         <div class="book-info">
             <h3>${book.title}</h3>
-            <p>${book.author}</p>
+            <p class="book-author">${book.author}</p>
             <p>Available in store and online</p>
             <p>Delivery info</p>
             <h3>${book.price} SEK</h3>
         </div>
+
         <div class="icons-column">
             <div class="cart-icons">
                 <i class="fa-regular fa-heart"></i> <span>
                 <i class="fa-solid fa-trash-can"></i>
             </div>
+
             <div class="count-icons">
                 <i class="fa-regular fa-square-minus"></i>
-                <span> 1 </span>
+                <span> ${book.count || 1} </span>
                 <i class="fa-regular fa-square-plus"></i>
             </div>
         </div>`;
         
         container.appendChild(item);
+
+        // Add book price to total
+        const price = book.price || 0;
+        const count = book.count || 1;
+        total += price * count;
     });
-  
+    // Display total in the totalEl
+    if (totalEl) {
+        totalEl.textContent = total; 
+    }
 });
