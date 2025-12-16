a99c4442bc09326f9b9388b9ec61afbe2bd7466c
Ebrima-Sarge <Ebrima-Sarge@users.noreply.github.com>
2025-12-16 12:38:37 +0000

    deploy: b28019d58092eadb413fdd032699629c0ab6bca4

--- assets/wishlist-C2820sm6.js
+++
@@ -1,1 +0,0 @@
-import{c as t,u as a}from"./scrollToTop-z7LA5Z2K.js";window.addEventListener("DOMContentLoaded",()=>{t(),a()});
---
+++ assets/wishlist-DnZFO1oV.js
@@ -0,0 +1,1 @@
+import{c as t,u as a}from"./scrollToTop-NP_7qT9i.js";window.addEventListener("DOMContentLoaded",()=>{t(),a()});
